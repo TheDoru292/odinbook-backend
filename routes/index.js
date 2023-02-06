a6266c3e107fa90bd820b4a2322c0f14901d42ca
thedoru <thedoruv2@gmail.com>
2023-02-06 17:45:02 +0100

    added routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+require("dotenv").config();
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGODB_LINK);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const usersRouter = require("./users");
+const authRouter = require("./auth");
+
+router.use("/user", usersRouter);
+
+router.use("/auth", authRouter);
 
 module.exports = router;
