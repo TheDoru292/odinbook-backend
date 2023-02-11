6413fae31b1e3803e335bb5181dc46abe4a72ccc
thedoru <thedoruv2@gmail.com>
2023-02-11 14:34:02 +0100

    post route additions

--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const comment = require("../controllers/commentController");
 const post = require("../controllers/postController");
+const helper = require("../lib/helper");
 
 const passport = require("passport");
 require("../passport");
@@ -10,10 +12,35 @@ router.get("/", post.get);
 
 router.post("/", passport.authenticate("jwt", { session: false }), post.create);
 
+router.get("/:postId", post.getOne);
+
 router.delete(
   "/:postId",
   passport.authenticate("jwt", { session: false }),
+  helper.checkIfPostExists,
+  helper.checkSameUserPost,
   post.delete
+);
+
+router.put(
+  "/:postId",
+  passport.authenticate("jwt", { session: false }),
+  helper.checkIfPostExists,
+  helper.checkSameUserPost,
+  post.edit
+);
+
+router.get(
+  "/:postId/comment",
+  helper.checkIfPostExists,
+  comment.getAllPostComments
+);
+
+router.post(
+  "/:postId/comment",
+  passport.authenticate("jwt", { session: false }),
+  helper.checkIfPostExists,
+  comment.post
 );
 
 module.exports = router;
