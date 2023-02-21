94637e6242780b125360cb47f16c560450858c27
thedoru <thedoruv2@gmail.com>
2023-02-21 17:18:41 +0100

    friend contoller

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -1,6 +1,19 @@
 const friend = require("../models/friend");
 const friendReq = require("../models/friendReq");
+const async = require("async");
 const ErrorHandler = require("../lib/ErrorHandler");
+const mongoose = require("mongoose");
+
+exports.getUserFriends = (req, res) => {
+  friend.find({ first_user: req.params.userId }, (err, friends) => {
+    if (err) {
+      const Error = new ErrorHandler(err, 500);
+      return res.status(Error.errCode).json(Error.error);
+    }
+
+    return res.status(200).json({ success: true, friends });
+  });
+};
 
 exports.post = (req, res) => {
   const friendReqObj = {
@@ -17,4 +30,158 @@ exports.post = (req, res) => {
 
     return res.status(200).json({ success: true, friendReq });
   });
+};
+
+exports.getFriendRequests = (req, res) => {
+  friendReq.find({ recipient: req.params.userId }, (err, friendReqs) => {
+    if (err) {
+      const Error = new ErrorHandler(err, 500);
+      return res.status(Error.errCode).json(Error.error);
+    }
+
+    return res.status(200).json({ success: true, friendReqs });
+  });
+};
+
+exports.acceptFriendRequest = (req, res) => {
+  friendReq.findOne({ _id: req.params.requestId }, (err, friendReqDone) => {
+    if (err) {
+      const Error = new ErrorHandler(err, 500);
+      return res.status(Error.errCode).json(Error.error);
+    }
+
+    async.waterfall(
+      [
+        function (callback) {
+          const friendObj = {
+            first_user: friendReqDone.sender,
+            second_user: friendReqDone.recipient,
+            friends_since: new Date(),
+          };
+
+          friend.create(friendObj, (err, friendDone) => {
+            if (err) {
+              callback(err);
+            }
+
+            callback(null, friendDone);
+          });
+        },
+        function (arg1, callback) {
+          const friendObj = {
+            first_user: friendReqDone.recipient,
+            second_user: friendReqDone.sender,
+            friends_since: new Date(),
+          };
+
+          friend.create(friendObj, (err, friendDone) => {
+            if (err) {
+              callback(err);
+            }
+
+            callback(null, friendDone);
+          });
+        },
+      ],
+      function (err, results) {
+        if (err) {
+          const Error = new ErrorHandler(err, 500);
+          return res.status(Error.errCode).json(Error.error);
+        }
+
+        friendReq.findOneAndDelete(
+          { _id: req.params.requestId },
+          (err, deleted) => {
+            if (err) {
+              const Error = new ErrorHandler(err, 500);
+              return res.status(Error.errCode).json(Error.error);
+            }
+
+            return res
+              .status(200)
+              .json({ success: true, status: "You are now friends!" });
+          }
+        );
+      }
+    );
+  });
+};
+
+exports.deleteFriendRequest = (req, res) => {
+  friendReq.findOneAndDelete({ _id: req.params.requestId }, (err, request) => {
+    if (err) {
+      const Error = new ErrorHandler(err, 500);
+      return res.status(Error.errCode).json(Error.error);
+    }
+
+    return res
+      .status(200)
+      .json({ success: true, status: "Friend request denied." });
+  });
+};
+
+exports.getOutgoingFriendRequests = (req, res) => {
+  friendReq.find({ sender: req.params.userId }, (err, friendReqs) => {
+    if (err) {
+      const Error = new ErrorHandler(err, 500);
+      return res.status(Error.errCode).json(Error.error);
+    }
+
+    return res.status(200).json({ success: true, friendReqs });
+  });
+};
+
+exports.deleteOutgoingFriendRequest = (req, res) => {
+  friendReq.findByIdAndDelete(
+    { _id: req.params.requestId },
+    (err, friendReq) => {
+      if (err) {
+        const Error = new ErrorHandler(err, 500);
+        return res.status(Error.errCode).json(Error.error);
+      }
+
+      return res
+        .status(200)
+        .json({ success: true, status: "Friend request deleted." });
+    }
+  );
+};
+
+exports.removeFriend = (req, res) => {
+  async.waterfall(
+    [
+      function (callback) {
+        friend.findOneAndDelete(
+          { first_user: req.params.userId, second_user: req.params.friendId },
+          (err, friend) => {
+            if (err) {
+              callback(err);
+            }
+
+            callback(null, friend);
+          }
+        );
+      },
+      function (arg1, callback) {
+        friend.findOneAndDelete(
+          { first_user: req.params.friendId, second_user: req.params.userId },
+          (err, friend) => {
+            if (err) {
+              callback(err);
+            }
+
+            callback(null, friend);
+          }
+        );
+      },
+    ],
+    function (err, results) {
+      if (err) {
+        const Erorr = new ErrorHandler(err, 500);
+        return res.status(Error.errCode).json(Error.error);
+      }
+
+      return res.status(200).json({ success: true, status: "Friend removed." });
+    }
+  );
 };
