ed39b1e52c7ca18c8568be697fb8b841d0197d22
thedoru <thedoruv2@gmail.com>
2023-02-27 23:57:46 +0100

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const apiRouter = require("./routes/index");
 
@@ -12,11 +13,17 @@ const app = express();
 
 // view engine setup
 
+const corsOptions = {
+  origin: "http://localhost:3001",
+  optionsSuccessStatus: 200
+}
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/api", apiRouter);
 
