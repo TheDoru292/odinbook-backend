6b91b7f6ab9ee437f07978c54f8f695df6b22ac0
thedoru <thedoruv2@gmail.com>
2023-03-09 22:11:41 +0100

    limit posts to 999

--- controllers/postController.js
+++ controllers/postController.js
@@ -6,9 +6,13 @@ const like = require("../models/like");
 const comment = require("../models/comment");
 
 exports.get = (req, res) => {
+  const { page = 1, limit = 999 } = req.query;
+
   post.paginate(
     {},
     {
+      page,
+      limit,
       select: "_id",
     },
     (err, posts) => {
